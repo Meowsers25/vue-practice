138d01a4b0473fbdb7bce905386f3511c3bceb7f
Chris <c.lamothe25@gmail.com>
2019-02-13 21:49:49 -0500

    Using v-bind to bind data

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue ({
     el: '#app',
     data: {
         title: 'Let\'s learn Vue',
-        name: 'Katie'
+        name: 'Katie',
+        url: 'http://www.youtube.com',
+        classes: ['one', 'two']
     },
     methods: {
         greet(time) {
